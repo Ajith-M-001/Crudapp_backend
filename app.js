97df4cbcead51a950b8c375eb0586ec7be72892b
Ajith-M-001 <ajithguru741@gmail.com>
2023-11-22 17:36:52 +0530

    connected to mongodb database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import connectDB from "./database/db.js";
 
 const app = express();
 const PORT = 5000;
@@ -7,6 +8,15 @@ app.get("/", (req, res) => {
   res.send("server started");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on the port http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server running on the port http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error connecting to mongodb database ${error.message}`);
+  }
+};
+
+startServer();
