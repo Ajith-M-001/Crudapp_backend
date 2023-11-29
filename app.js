69dc67b5aa93de49b1822e37e54e41b0d10a7477
Ajith-M-001 <ajithguru741@gmail.com>
2023-11-29 11:43:04 +0530

    chaned for production

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import express from "express";
 import connectDB from "./database/db.js";
 import userRouter from "./Router/userRouter.js";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
